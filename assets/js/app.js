99a3bc8021cb24a2ff26b9456c01b6a8559169d7
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-12-08 23:35:59 +0200

    add navbar design and logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,19 @@
 "use strict";
 // DOM elements
-
+const navLinksParent = document.querySelector("ul.navbar-nav");
 // arrays
 
 // events
+navLinksParent.addEventListener("click", function (e) {
+  styleClickedLink(e.target);
+});
 
 // functions
+function styleClickedLink(clickedLink) {
+  if (clickedLink.classList.contains("nav-link")) {
+    document.querySelectorAll(".nav-link").forEach((link) => {
+      link.classList.remove("active");
+    });
+    clickedLink.classList.add("active");
+  }
+}
