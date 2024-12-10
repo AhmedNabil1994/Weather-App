45a762188140b7396e70e7c92bc624b91a5fae52
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-12-11 01:11:11 +0200

    add city as a param and add res as a param also in both fns

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,7 @@
 const navLinksParent = document.querySelector("ul.navbar-nav");
 const weatherCardsContainer = document.querySelector("#weather .row");
 // variables
-const url =
-  "http://api.weatherapi.com/v1/forecast.json?key=d185b75d38f2481da10172021241012&q=cairo&days=3";
-let weatherDetails;
+
 // events
 navLinksParent.addEventListener("click", function (e) {
   styleClickedLink(e.target);
@@ -21,7 +19,7 @@ function styleClickedLink(clickedLink) {
   }
 }
 
-function displayCards() {
+function displayCards(weatherData) {
   let cards = "";
   for (let index = 0; index < 3; index++) {
     if (index === 0) {
@@ -29,38 +27,38 @@ function displayCards() {
             <div class="card card_${index + 1}">
               <div class="card-header d-flex justify-content-between">
                 <p class="m-0">${new Date(
-                  weatherDetails.forecast.forecastday[index].date
+                  weatherData.forecast.forecastday[index].date
                 ).toLocaleString("en-US", { weekday: "long" })}</p>
                 <p class="m-0">${new Date(
-                  weatherDetails.forecast.forecastday[index].date
+                  weatherData.forecast.forecastday[index].date
                 ).getDate()}${new Date(
-        weatherDetails.forecast.forecastday[index].date
+        weatherData.forecast.forecastday[index].date
       ).toLocaleString("en-US", { month: "long" })}</p>
               </div>
               <div class="card-body py-5">
-                <h5 class="card-title">${weatherDetails.location.name}</h5>
-                <p class="card-text">${weatherDetails.current.temp_c}&deg;C</p>
+                <h5 class="card-title">${weatherData.location.name}</h5>
+                <p class="card-text">${weatherData.current.temp_c}&deg;C</p>
                 <div class="d-flex justify-content-between align-items-center status">
-                  <img src="${weatherDetails.current.condition.icon}" alt="${
-        weatherDetails.current.condition.text
+                  <img src="${weatherData.current.condition.icon}" alt="${
+        weatherData.current.condition.text
       }">
-                  <p class="m-0">${weatherDetails.current.condition.text}</p>
+                  <p class="m-0">${weatherData.current.condition.text}</p>
                 </div>
                 <div class="icons d-flex">
                   <div class="me-2">
                     <i class="fa-solid fa-umbrella"></i>
                     <span>${
-                      weatherDetails.forecast.forecastday[index].day
+                      weatherData.forecast.forecastday[index].day
                         .daily_chance_of_rain
                     }%</span>
                   </div>
                   <div class="me-2">
                     <i class="fa-solid fa-wind"></i>
-                    <span>${weatherDetails.current.wind_kph}km/h</span>
+                    <span>${weatherData.current.wind_kph}km/h</span>
                   </div>
                   <div>
                     <i class="fa-solid fa-compass"></i>
-                    <span>${weatherDetails.current.wind_dir}</span>
+                    <span>${weatherData.current.wind_dir}</span>
                   </div>
                 </div>
               </div>
@@ -71,27 +69,27 @@ function displayCards() {
             <div class="card card_${index + 1} text-center">
               <div class="card-header">
                 <p class="m-0">${new Date(
-                  weatherDetails.forecast.forecastday[index].date
+                  weatherData.forecast.forecastday[index].date
                 ).toLocaleString("en-US", { weekday: "long" })}</p>
               </div>
               <div class="card-body py-5">
                 <h5 class="card-title">
                   <img src="${
-                    weatherDetails.forecast.forecastday[index].day.condition
+                    weatherData.forecast.forecastday[index].day.condition
                       .icon
                   }" alt="${
-        weatherDetails.forecast.forecastday[index].day.condition.text
+        weatherData.forecast.forecastday[index].day.condition.text
       }">
                 </h5>
                 <p class="card-text">${
-                  weatherDetails.forecast.forecastday[index].day.maxtemp_c
+                  weatherData.forecast.forecastday[index].day.maxtemp_c
                 }&deg;C</p>
                 <div class="status">
                   <p class="">${
-                    weatherDetails.forecast.forecastday[index].day.mintemp_c
+                    weatherData.forecast.forecastday[index].day.mintemp_c
                   }&deg;</p>
                   <p class="m-0">${
-                    weatherDetails.forecast.forecastday[index].day.condition
+                    weatherData.forecast.forecastday[index].day.condition
                       .text
                   }</p>
                 </div>
@@ -103,16 +101,20 @@ function displayCards() {
   weatherCardsContainer.innerHTML = cards;
 }
 
-(function getWeather() {
+getWeather("cairo");
+function getWeather(city) {
   let req = new XMLHttpRequest();
-  req.open("GET", url);
+  req.open(
+    "GET",
+    `http://api.weatherapi.com/v1/forecast.json?key=d185b75d38f2481da10172021241012&q=${city}&days=3`
+  );
   req.send();
   req.responseType = "json";
   req.addEventListener("load", () => {
     if (req.status >= 200 && req.status < 300) {
-      weatherDetails = req.response;
+      let weatherDetails = req.response;
       console.log(weatherDetails);
-      displayCards();
+      displayCards(weatherDetails);
     }
   });
-})();
+}
