69317ace031a6cfb435aab410b1475f1dc4a1383
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-12-10 16:46:17 +0200

    design and display weather cards static

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 "use strict";
 // DOM elements
 const navLinksParent = document.querySelector("ul.navbar-nav");
+const weatherCardsContainer = document.querySelector("#weather .row");
+console.log(weatherCardsContainer);
 // arrays
 
 // events
@@ -17,3 +19,62 @@ function styleClickedLink(clickedLink) {
     clickedLink.classList.add("active");
   }
 }
+
+function displayCards() {
+  let cardEls = "";
+  for (let index = 0; index < 3; index++) {
+    if (index === 0) {
+      cardEls += ` <div class="col-md-6 col-lg-4 ">
+            <div class="card card_${index+1}">
+              <div class="card-header d-flex justify-content-between">
+                <p class="m-0">Monday</p>
+                <p class="m-0">9December</p>
+              </div>
+              <div class="card-body py-5">
+                <h5 class="card-title">Cairo</h5>
+                <p class="card-text">23.1oC</p>
+                <div class="d-flex justify-content-between status">
+                  <i class="fa-solid fa-sun"></i>
+                  <p>Sunny</p>
+                </div>
+                <div class="icons d-flex">
+                  <div class="me-2">
+                    <i class="fa-solid fa-umbrella"></i>
+                    <span>20%</span>
+                  </div>
+                  <div class="me-2">
+                    <i class="fa-solid fa-wind"></i>
+                    <span>18km/h</span>
+                  </div>
+                  <div>
+                    <i class="fa-solid fa-compass"></i>
+                    <span>East</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    } else {
+      cardEls += `<div class="col-md-6 col-lg-4">
+            <div class="card card_${index+1} text-center">
+              <div class="card-header">
+                <p class="m-0">Tuesday</p>
+              </div>
+              <div class="card-body py-5">
+                <h5 class="card-title">
+                  <i class="fa-solid fa-cloud-sun"></i>
+                </h5>
+                <p class="card-text">23.1oC</p>
+                <div class="status">
+                  <p class="">14.1o</p>
+                  <p class="m-0">Partly Cloudy</p>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    }
+  }
+  weatherCardsContainer.innerHTML = cardEls
+}
+
+displayCards();
